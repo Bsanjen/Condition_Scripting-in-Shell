1a2be8daa48544b7b72595a122e99ca8b2ad9e41
Sanjen <85753404+Bsanjen@users.noreply.github.com>
2024-09-25 21:19:46 +0530

    Create Condition_view.sh

---
+++ Condition_view.sh
@@ -0,0 +1,17 @@
+read -p "What is your Country..? : " name
+read -p "Who is PM of India..? : " name1
+
+if [[ $name == "India" ]];
+
+then
+        echo " You are correct Bro "
+
+elif
+       [[ $name1 == "Modi" ]];
+then
+
+       echo "you are correct Bro "
+else
+        echo " Sorry dude. You are wrong "
+
+fi
